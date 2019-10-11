efc9edb9fa33e68e130168d35da652e78b9a0621
Crawford <David.Crawford3@jacobs.com>
2019-10-11 13:00:08 -0400

    api query functionality

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -3,16 +3,21 @@ var router = express.Router();
 const request = require('superagent');
 
 router.get('/', function(req, response, next) {
-    var payload = GetCMSPayload(response);    
+    // for (query in req.query){
+    //     console.log(JSON.stringify(query))
+    // }
+    console.log(req.query.type)
+    var payload = GetCMSPayload(req.query.type, response);    
 });
 
-function GetCMSPayload(response){
+function GetCMSPayload(query, response){
     request
         .get('https://apps.elegantcms.io/api/v1/contents')
         .set('Authorization', "Token token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBfdXVpZCI6ImY2OGE5YWUxLTJmOTgtNGQ1MC05YTBmLWY5YzQ1NzQ2MDAwOSIsImlkIjoxMzQxLCJ1cGRhdGVkX2F0IjoiMjAxOS0xMC0wOCAxNzoyMToyNSBVVEMifQ.nb06rtKF8vRTycyOG48KdNP1oZ92SuzJtax8eS6WJuY")
+        .query("filter[type]="+query)
         .then(function (res){
             _data = JSON.parse(res.text);
-            // console.log(_data.data)
+            // console.log(_data)
             _dataObj = _data.data[0].attributes.fields;
             // for (var i in _data.data){
             //     console.log(_data.data[i].attributes)
@@ -21,7 +26,7 @@ function GetCMSPayload(response){
             _dataStringified = JSON.stringify(_dataObj)
             // console.log(res)
             // res.send(_dataText);
-            // console.log(_dataStringified)
+            console.log(_dataStringified)
             response.send(_dataStringified)
             // console.log(_dataObj.title)
         })

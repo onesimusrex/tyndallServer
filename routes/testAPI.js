15e1fdb1ac3a1c55945be7e9c76428fd709ef9ad
Crawford <David.Crawford3@jacobs.com>
2019-10-17 17:13:45 -0400

    working on integrating data

--- routes/testAPI.js
+++ routes/testAPI.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const request = require('superagent');
+var ifsData = require('../utils/dataUtil1.js')
 
 router.get('/', function(req, response, next) {
     var payload = GetCMSPayload(req.query.type, response);    
@@ -12,19 +13,32 @@ function GetCMSPayload(query, response){
         .set('Authorization', "Token token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhcHBfdXVpZCI6ImY2OGE5YWUxLTJmOTgtNGQ1MC05YTBmLWY5YzQ1NzQ2MDAwOSIsImlkIjoxMzQxLCJ1cGRhdGVkX2F0IjoiMjAxOS0xMC0wOCAxNzoyMToyNSBVVEMifQ.nb06rtKF8vRTycyOG48KdNP1oZ92SuzJtax8eS6WJuY")
         .query("filter[type]="+query)
         .then(function (res){
-            _data = JSON.parse(res.text);
-            // console.log(_data)
-            _dataObj = _data.data[0].attributes.fields;
-            // for (var i in _data.data){
-            //     console.log(_data.data[i].attributes)
-            // } 
-            // console.log(_dataObj)
-            _dataStringified = JSON.stringify(_dataObj)
-            // console.log(res)
-            // res.send(_dataText);
-            console.log(_dataStringified)
-            response.send(_dataStringified)
-            // console.log(_dataObj.title)
+
+            if (query == 'pdfs'){
+                request
+                    .get(JSON.parse(res.text).data[0].attributes.fields.ifspdf.url)
+                    .then(function(txtfile){
+                        // console.log(txtfile.text)
+                        console.log("process" + ifsData.init(txtfile.text, response));
+                        // response.send(JSON.stringify({text: txtfile}))
+                        response.send( JSON.stringify({json: ifsData.init(txtfile.text)} ) )
+                    })
+            } else {
+                _data = JSON.parse(res.text);
+                // console.log(_data)
+                _dataObj = _data.data[0].attributes.fields;
+                // for (var i in _data.data){
+                //     console.log(_data.data[i].attributes)
+                // } 
+                // console.log(_dataObj)
+                _dataStringified = JSON.stringify(_dataObj)
+                // console.log(res)
+                // res.send(_dataText);
+                // console.log(_dataStringified)
+                response.send(_dataStringified)
+                // console.log(_dataObj.title)
+            }
+
         })
 }
 
---
+++ utils/dataUtil1.js
@@ -0,0 +1,191 @@
+function ifsData () {
+    this.init = init;
+    this.data = {};
+    this.ElimCategoryText = ElimCategoryText;
+    this.GetHeadings = GetHeadings;
+    this.GetKeywordRelevance = GetKeywordRelevance;
+    this.AddNlpFeature = AddNlpFeature;
+    this.ProcessIFSData = ProcessIFSData;
+}
+
+function init (ifsText){
+    str = "\`"+ifsText+"\`"
+    superagent = require ('superagent');
+    const NaturalLanguageUnderstandingV1 = require ('ibm-watson/natural-language-understanding/v1');
+    const { IamAuthenticator } = require ('ibm-watson/auth');
+
+    const naturalLanguageUnderstanding = new NaturalLanguageUnderstandingV1({
+        version: '2019-07-12',
+        authenticator: new IamAuthenticator({ apikey: 'Asrl_esdUauoAsbVmTG5KxM8BaiqCpBo2rpqY08XCSqN'}),
+        url: 'https://gateway.watsonplatform.net/natural-language-understanding/api'
+    })
+
+
+    // returns patterns that matche 6 digit csi code and document header
+    var csiPattern = /([\n][\d]{2}[\s][\d]{2}[\s][\d]{2})/gm;
+    var headerPattern = /[\w]*[\s]*[\d]*[\s]*IFS Design Guide Appendix \| Rough Draft\s*\w*\s*\d*\s*\d*\s*[\w]*[\s]*[\d]*[\s]*[\d]*/gm
+    var divisionHeading = /(DIVISION[\s]*[\d]*(?:[\s]*[A-Z]*(?:,)?)*[\s])/gm
+
+    words = str.split(csiPattern);
+    words = words.slice(1);
+    data = []        
+
+    for (i=0;i<words.length;i=i+2){
+        if (i == 0){
+            var header1 = words[i].slice(1).split("\n")
+            var headerBod = header1.slice(1).join(" ").split(headerPattern).join("")
+            heading1 = this.GetHeadings(headerBod, divisionHeading)
+            if (heading1){
+                data.push(heading1);
+            }
+        }
+        
+        var text = words[i+1].slice(1).split("\n")
+        var body = text.slice(1).join(" ").split(headerPattern).join("")
+        
+        words[i] = words[i].slice(1); 
+        data.push({
+            type: "subcategory",
+            csi: words[i],
+            l1: words[i].slice(0,2),
+            l2: words[i].slice(3,5),
+            l3: words[i].slice(6),
+            title: text[0],
+            body: body
+        })
+        //delete redundant heading text
+        heading = this.GetHeadings(body, divisionHeading)
+        if (heading){
+            data.push(heading);
+        }
+    }
+
+
+    this.ElimCategoryText(data)
+
+    this.ProcessIFSData(naturalLanguageUnderstanding);
+
+}
+
+function ElimCategoryText(_data){
+    for (var i=0; i<_data.length; i++){
+        if (_data[i].type == "category"){
+            bodySplit = _data[i-1].body.split(divisionHeading);
+            _data[i-1] = bodySplit[0].trim();
+            _word = _data[i].csi = _data[i+1].csi;
+            _data[i].l1 = _word.slice(0,2)
+            _data[i].l2 = _word.slice(3,5)
+            _data[i].l3 = _word.slice(6)
+        }
+    }
+}
+
+function GetHeadings (body, _divisionHeading ){
+    if (_divisionHeading.test(body)){
+        bodySplit = body.split(_divisionHeading);
+        body = bodySplit[0]
+        var _text = bodySplit.slice(-1).join("")
+        var headingArr = bodySplit.slice(1,-1).join("")
+        if (_text.split(" ").length == 1){
+            _text = "";
+        }
+        return {
+            type: "category",
+            csi: null,
+            title: headingArr.trim(),
+            body: _text
+        };
+    }
+}
+
+function AddNlpFeature(_analysisResults){
+    var ibmReturnTime = new Date() 
+    // console.log("send time: " +ibmReturnTime.toString())    
+    nlpData = {};
+    featureTypes = ["keywords", /*"entities",*/ "concepts"]
+    // console.log(_analysisResults["keywords"])
+    for (var i=0; i<featureTypes.length; i++){
+        var nlpType = nlpData[featureTypes[i]] = []
+        // console.log(nlpType)
+        var ft = featureTypes[i]
+        for (var j=0; j<_analysisResults[featureTypes[i]].length; j++){
+            var featureItem = _analysisResults[featureTypes[i]][j];
+            var tempObj = {
+                text: featureItem.text,
+                relevance: featureItem.relevance,
+                
+            }
+            if (featureTypes[i] == "entities"){
+                tempObj["type"] = featureItem.type
+                tempObj["count"] = featureItem.count
+                tempObj["mentions"] = []
+                for(var k=0; k<featureItem.mentions.length; k++){
+                    tempObj["mentions"].push({
+                        text: featureItem.mentions[k].text,
+                        location: [featureItem.mentions[k].location[0], featureItem.mentions[k].location[1]],
+                        confidence: featureItem.mentions[k].confidence
+                    })
+                }
+            }
+            nlpType.push(tempObj)
+        }
+    }
+    return nlpData;
+}
+
+function ProcessIFSData(naturalLanguageUnderstanding) {
+    for (var i=0; i<data.length; i++){
+        if (data[i].csi == '08 71 00'){
+            //prints the data item
+            //console.log(data[i].csi + '\n' + data[i].title + '\n' + data[i].body)
+            return this.GetKeywordRelevance(data[i].body, 3, naturalLanguageUnderstanding);
+        }
+        
+    }
+}
+
+function GetKeywordRelevance(_text, num, nlu){
+    var analyzeParams = {
+        // 'url': 'www.nytimes.com',
+        text: _text,
+        'features': {
+            'keywords': {
+                'limit': num
+            },
+            'relations': {},
+            'entities': {
+                'limit': num,
+                'mentions': true
+            },
+            'concepts': {
+                'limit': num
+            }
+        }
+    }
+    var ibmSendTime = new Date() 
+    // console.log("send time: " +ibmSendTime.toString())
+    _this = this;
+    nlu.analyze(analyzeParams)
+        .then(analysisResults => {
+            // console.log(JSON.stringify(analysisResults.result, null, 2))
+            // console.log(this)
+            console.log(_this.AddNlpFeature(analysisResults.result));
+            // return _this.AddNlpFeature(analysisResults.result);
+        })
+        .catch(err => {
+            console.log('error', err);
+        })
+}
+
+//loads text data
+
+module.exports = new ifsData();
+
+
+
+
+
+
+
+// console.log(AddNlpFeature())
+// console.log(data)
